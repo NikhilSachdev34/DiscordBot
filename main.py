d879656073b8e6061ccc991b015b2f46eb6eda6b
eldym <eldymoddys@gmail.com>
2025-01-22 09:10:42 -0500

    Basic Discord bot imports, config constant variables

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import discord
+import asyncio
+
+from discord.ext import tasks, commands
+from config import TOKEN, PREFIXES
+
+client = commands.Bot(command_prefix = PREFIXES, intents=discord.Intents.all(), help_command=None) # Creates our bot
+
 print("Hello World")
+
+client.run(TOKEN) # Runs client
